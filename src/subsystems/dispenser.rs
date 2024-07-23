7c9ce194c1a99170a2a49809cecc00db6ef9b6b5
Riley <rileykhernandez@gmail.com>
2024-07-23 13:25:15 -0700

    shortened checking time for dispense

--- src/subsystems/dispenser.rs
+++ src/subsystems/dispenser.rs
@@ -150,7 +150,6 @@ impl Dispenser {
                 let target_weight = init_weight - w.setpoint;
 
                 //Starting motor moves
-                //Run backwards for a bit
                 self.motor.set_velocity(self.parameters.motor_speed).await;
                 if let Some(retract) = self.parameters.retract_before {
                     self.motor.relative_move(-retract).await.expect("Motor faulted");
@@ -201,7 +200,7 @@ impl Dispenser {
                         //     self.motor.relative_move(-retract).await.unwrap();
                         //     self.motor.wait_for_move(Duration::from_millis(50)).await.unwrap();
                         // }
-                        let check_weight = self.get_median_weight(150, self.parameters.sample_rate).await;
+                        let check_weight = self.get_median_weight(50, self.parameters.sample_rate).await;
                         if check_weight < target_weight + self.parameters.stop_offset {
                             if let Some(retract) = self.parameters.retract_after {
                                 self.motor.relative_move(-retract).await.unwrap();
