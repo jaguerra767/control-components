12e8c7de252a9bc9e04987d71704c196b5a88545
Riley <rileykhernandez@gmail.com>
2024-07-29 11:03:15 -0700

    decreased gaps in dispense cycle check weight loop

--- src/subsystems/dispenser.rs
+++ src/subsystems/dispenser.rs
@@ -200,7 +200,7 @@ impl Dispenser {
                         //     self.motor.relative_move(-retract).await.unwrap();
                         //     self.motor.wait_for_move(Duration::from_millis(50)).await.unwrap();
                         // }
-                        let check_weight = self.get_median_weight(50, self.parameters.sample_rate).await;
+                        let check_weight = self.get_median_weight(30, self.parameters.sample_rate).await;
                         if check_weight < target_weight + self.parameters.stop_offset {
                             if let Some(retract) = self.parameters.retract_after {
                                 self.motor.set_velocity(self.parameters.motor_speed).await;
@@ -210,7 +210,7 @@ impl Dispenser {
                             break DispenseEndCondition::WeightAchieved(init_weight-check_weight)
                         }
                         self.motor.relative_move(10.).await.unwrap();
-                        tokio::time::sleep(Duration::from_millis(250)).await
+                        tokio::time::sleep(Duration::from_millis(500)).await
                     }
                 };
                 self.motor.abrupt_stop().await;
