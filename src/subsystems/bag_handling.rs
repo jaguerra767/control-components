1d8b3e247a64a4b84dfffa977d6d3f5a018ded60
Riley <rileykhernandez@gmail.com>
2024-07-29 15:59:53 -0700

    upped pull_back speed, lowered distance

--- src/subsystems/bag_handling.rs
+++ src/subsystems/bag_handling.rs
@@ -63,8 +63,8 @@ impl BagDispenser {
         Ok(())
     }
     pub async fn pull_back(&self) -> Result<(), Box<dyn Error>> {
-        self.motor.set_velocity(1.5).await;
-        self.motor.relative_move(-4.5).await.unwrap();
+        self.motor.set_velocity(1.0).await;
+        self.motor.relative_move(-4.4).await.unwrap();
         while self.motor.get_status().await == Status::Moving {
             sleep(Duration::from_millis(100)).await;
         }
